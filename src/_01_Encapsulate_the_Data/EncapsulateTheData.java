00e8ea7a14ff73f785238042272bd639b2c328e4
root <root@8d0676d3d147>
2021-11-21 21:00:07 +0000

    Finished intro to encapsulation and switch statements, on data type option pane number 6 on directions, look to class to see what need to do

--- src/_01_Encapsulate_the_Data/EncapsulateTheData.java
+++ src/_01_Encapsulate_the_Data/EncapsulateTheData.java
@@ -1,13 +1,30 @@
 package _01_Encapsulate_the_Data;
 
 public class EncapsulateTheData {
-
+	
+	
+	
+		
+		
+	
     /*
      * itemsReceived cannot negative. All negative parameters should set
      * itemsReceived to 0.
      */
 
-    int itemsReceived;
+    private int itemsReceived;
+    public void setItemsReceived(int number) {
+		if(number<0) {
+			this.itemsReceived=0;
+		}
+		else {
+    	this.itemsReceived=number;
+		}
+	}
+	
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
 
     /*
      * degreesTurned must be locked between 0.0 and 360.0 inclusive. All
@@ -15,20 +32,59 @@ public class EncapsulateTheData {
      * bound.
      */
 
-    double degreesTurned;
-
+   private double degreesTurned;
+    public void setDegreesTurned(double number) {
+    	if(number>=0.0 && number<=360.0) {
+    		this.degreesTurned=number;
+    	}
+    	else {
+    		if(number<0.0) {
+    			this.degreesTurned=0.0;
+    		}
+    		else if(number>360.0) {
+    			this.degreesTurned=360.0;
+    		}
+    	}
+    }
+    public double getDegreesTurned() {
+    	return degreesTurned;
+    }
     /*
      * nomenclature must not contain an empty String. An empty String parameter
      * should set nomenclature to a String with a single space.
      */
 
-    String nomenclature;
+    private String nomenclature;
+    public void setNomenclature(String input) {
+    	if(!input.isEmpty()) {
+    		this.nomenclature=input;
+    	}
+    	else {
+    		String space=" ";
+    		this.nomenclature=space;
+    	}
+    }
+    public String getNomenclature() {
+    	return nomenclature;
+    }
 
     /*
      * memberObj must not be a String. A String parameter should set memberObj
      * to a new Object(); Hint: Use the instanceof operator.
      */
 
-    Object memberObj;
+    private Object memberObj;
+    public void setMemberObj(Object object) {
+    	if(object instanceof String) {
+    		this.memberObj=new Object();
+    	}
+    	else {
+    		this.memberObj=object;
+    	}
+    }
+    public Object getMemberObj() {
+    	return memberObj;
+    }
+	
 
 }
--- src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
+++ src/_04_Data_Type_Option_Pane/DataTypeOptionPane.java
@@ -3,8 +3,25 @@ package _04_Data_Type_Option_Pane;
 import javax.swing.JOptionPane;
 
 public class DataTypeOptionPane {
+	
+	enum DataType{
+		BYTE(8), SHORT(16), INT(32), LONG(64), FLOAT(32), DOUBLE(64), BOOLEAN(1), CHAR(16);
+		private int bits;
+		
+		private DataType(int bits) {
+			this.bits=bits;
+		}
+		public int getBits() {
+			return this.bits;
+		}
+	}
     public static void main(String[] args) {
 
-        DataType[] dataTypes;
+        DataType[] dataTypes=DataType.values();
+        //ON NUMBER 6, NEED TO CREATE AN OPTION DIALOG USING THE DATATYPES ARRAY AS THE 
+        //PARAMETER ARRAY, LOOK AT THE LINK ON THE DIRECTIONS FOR HOW TO MAKE A OPTION DIALOG
+        //BASIALLY NEED TO DISPLAY THE DIFFERENT THINGS INSIDE THE DATATYPE ARRAY ON EACH BUTTON
+        //AND EACH ONE CORRESPONDS TO THE ENUM'S NUMBER OR SOMETHING LIKE THAT
     }
+    
 }
